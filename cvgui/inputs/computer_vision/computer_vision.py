96a872566806bfa0cfdcbbd44126c04823627934
mitchellss <stephenmitchell2299@gmail.com>
2023-03-30 12:10:58 -0400

    add  support for windows

--- cvgui/inputs/computer_vision/computer_vision.py
+++ cvgui/inputs/computer_vision/computer_vision.py
@@ -1,5 +1,7 @@
 """Generates poses based on a computer vision model and a frame input."""
+import cv2
 import numpy as np
+import multiprocessing as mp
 from cvgui.core.recieving.service import CVModel, FrameInput
 
 
@@ -18,6 +20,55 @@ class ComputerVisionPose:
         """
         self.frame_input: FrameInput = frame_input
         self.model: CVModel = model
+
+    def start(self, skeleton_queue: mp.Queue):
+        """
+        Starts two processes, one for capturing/displaying frame input data and
+        one for processing that frame input data to get skeletons out of them.
+        These are done as separate processes because otherwise the skeleton
+        processing greatly slows down the speed at which frames are collected,
+        resulting in video feedback that is "laggy".
+
+        Args:
+            skeleton_queue (multiprocessing.Queue): The queue to put skeleton
+                data into once it has been processed from frames.
+
+        Returns:
+            list[multiprocessing.Process]: All the processes started by this 
+                method so they can be closed correctly later down the line.
+        """
+        image_queue: mp.Queue = mp.Queue()
+        cap = mp.Process(target=self.capture_and_show, args=(image_queue,))
+        proc = mp.Process(target=self.process_image,
+                          args=(image_queue, skeleton_queue))
+        cap.start()
+        proc.start()
+        return [cap, proc]
+
+    def capture_and_show(self, image_queue):
+        """
+        Infinitely retrieves new frames and places them in the image
+        queue. Additionally, displays incoming frames to the user in
+        real-time.
+        """
+        while True:
+            frame = self.frame_input.get_frame()
+            image_queue.put(frame)
+            cv2.imshow("Video Input", frame)
+            wait_key = cv2.waitKey(1)
+            if wait_key == 27:
+                pass
+
+    def process_image(self, image_queue, skeleton_queue):
+        """
+        Infinitely takes images from the given queue and turns them into 
+        skeleton data using a computer vision model.
+        """
+        while True:
+            if image_queue.empty():
+                continue
+            skeleton = self.model.get_pose(image_queue.get())
+            skeleton_queue.put(skeleton)
 
     def get_pose(self) -> np.ndarray:
         """Uses the frame input and computer vision model in tandem
--- cvgui/inputs/computer_vision/frame_input/webcam.py
+++ cvgui/inputs/computer_vision/frame_input/webcam.py
@@ -1,4 +1,5 @@
 """FrameInput implementation for a computer webcam."""
+from typing import Any
 import numpy as np
 import cv2
 
@@ -13,22 +14,31 @@ class Webcam:
             device_num (int): The device number of the webcam. Try 0 if unsure.
             fps (int): The frames per second the webcam can provide.
         """
-        self.cap = cv2.VideoCapture(device_num)
-        self.cap.set(cv2.CAP_PROP_FPS, fps)
+        self.device_num = device_num
+        self.fps = fps
+        self.cap: cv2.VideoCapture = None
+
+    def _configure(self) -> None:
+        """
+        Creates and configures the capture object. 
+
+        This cannot be done in the init function because of how 
+        Windows handles multiprocessing.
+        """
+        self.cap = cv2.VideoCapture(self.device_num)
+        self.cap.set(cv2.CAP_PROP_FPS, self.fps)
         self.cap.set(cv2.CAP_PROP_FOURCC,
                      cv2.VideoWriter_fourcc("M", "J", "P", "G"))
 
     def get_frame(self) -> np.ndarray:
         """Gets a frame from the webcam."""
+        if self.cap is None:
+            self._configure()
+
         success: bool
         color_image: np.ndarray
         success, color_image = self.cap.read()
         if not success:
             return np.zeros(0)
         color_image = cv2.flip(color_image, 1)
-
-        cv2.imshow("Video Playback", color_image)
-        wait_key = cv2.waitKey(1)
-        if wait_key == 27:
-            pass
         return color_image
