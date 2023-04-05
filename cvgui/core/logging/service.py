d91d31811a617576346149a787536f08d48fd7b2
Stephen Mitchell <stephenmitchell2299@gmail.com>
2023-04-05 14:21:01 -0400

    mostly built logger class

--- cvgui/core/logging/service.py
+++ cvgui/core/logging/service.py
@@ -1,7 +1,24 @@
+import multiprocessing as mp
 
 class PoseLogger(Protocol):
 
     def start(self, pose_queue: mp.Queue) -> Iterable[mp.Process]:
         """
         Initializes the pose logger.
+        """
+
+    def log_data(self, pose_queue: mp.Queue) -> None:
+        """
+        Log data from the queue.
+        """
+    
+    def save(self) -> bool:
+        """
+        Save logged data to the disk.
+        """
+    
+    def close(self) -> bool:
+        """
+        Finish logging poses and safely close any
+        files or repositories.
         """
--- cvgui/outputs/loggers/csv_logger.py
+++ cvgui/outputs/loggers/csv_logger.py
@@ -1,24 +1,49 @@
+import numpy as np
 import multiprocessing as mp
 
 class CSVLogger:
 
+    data: np.ndarray
+    size: int
+    active: bool = True
+
     def __init__(self, filepath: Path):
         self.filepath = filepath
 
-    def start(self, mp.Queue) -> Iterable[mp.Process]:
-        cap = mp.Process(target=self.log_data)
+    def start(self, pose_queue: mp.Queue) -> Iterable[mp.Process]:
+        cap = mp.Process(target=self.log_data, args=pose_queue)
         return cap
     
-    def log_data(self):
-        # Create numpy array if not already created
-
+    def _configure(self, size):
+        self.data = np.empty((0,size))
+        self.size = size
+    
+    def log_data(self, pose_queue: mp.Queue) -> None:
         # Get data from queue and append it to numpy array
-        pass
+        while self.active:
+            if pose_queue.empty():
+                continue
+            pose_data = pose_queue.get()
+            newdata = pose_data.ravel()
+
+            # Create numpy array if not already created
+            if self.data is None:
+                data_length = newdata.shape[0]
+                self._configure(data_length)
+
+            self.data = np.vstack([self.data, newdata])
     
-    def save(self):
+    def _build_header(self) -> str:
+        header_array = []
+        for i in range(self.size//4):
+            header_array += [f"x{i:02d}",f"y{i:02d}",f"z{i:02d}",f"vis{i:02d}"]
+        return ",".join(header_array)
+    
+    def save(self) -> bool:
         # Save data to CSV
-        pass
+        header = self._build_header()
+        np.savetxt(self.filepath, self.data, header=header, delimiter=",", fmt="%5.5f")
     
-    def close(self):
+    def close(self) -> bool:
         # Finish writing to the file
-        pass
+        self.active = False
