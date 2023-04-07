6703e2068cadd79415bb3b96143be2aab8a98734
mitchellss <stephenmitchell2299@gmail.com>
2023-04-07 12:34:00 -0400

    add timestamp to csv logger data

--- cvgui/outputs/loggers/csv_logger.py
+++ cvgui/outputs/loggers/csv_logger.py
@@ -24,7 +24,7 @@ class CSVPoseLogger:
                 file should be saved.
         """
         self.filepath: Path = filepath
-        self.data: np.ndarray = np.empty((0, 132))
+        self.data: np.ndarray = None
         self.size: int = 0
         self.count = 0
         self._save_queue: mpq.Queue
@@ -50,7 +50,8 @@ class CSVPoseLogger:
         return [cap]
 
     def _configure(self, size) -> None:
-        self.data = np.empty((0, size))
+        # Add one to make room for the timestamp
+        self.data = np.empty((0, size + 1))
         self.size = size
 
     def _log_data(self, pose_queue: mpq.Queue, save_queue: mpq.Queue) -> None:
@@ -68,23 +69,25 @@ class CSVPoseLogger:
                 pose_data: np.ndarray = pose_queue.get()
                 newdata: np.ndarray = pose_data.ravel()
 
-                # insert timestamp into first index
-                newdata = np.insert(newdata, 0, [int(time.time())])
-
                 # Create numpy array if not already created
                 if self.data is None:
                     data_length: int = newdata.shape[0]
+                    self._configure(data_length)
 
-                    # Add one to make room for timestamp
-                    self._configure(data_length + 1)
-
+                # insert timestamp into first index
+                newdata = np.insert(newdata, 0, [time.time()])
                 self.data = np.vstack([self.data, newdata])
 
+                # Save the data if the save queue has been pushed
+                # to or if data hasn't been saved for 1 second.
                 if not save_queue.empty():
                     save_queue.get()
-                    header: str = self._build_header()
-                    np.savetxt(self.filepath, self.data, header=header,
-                               delimiter=",", fmt="%5.5f")
+                    self._save_to_csv()
+
+    def _save_to_csv(self) -> None:
+        header: str = self._build_header()
+        np.savetxt(self.filepath, self.data, header=header,
+                   delimiter=",", fmt="%5.5f", comments="")
 
     def _build_header(self) -> str:
         """
@@ -97,7 +100,7 @@ class CSVPoseLogger:
         for i in range(self.size//4):
             header_array += [f"x{i:02d}", f"y{i:02d}",
                              f"z{i:02d}", f"vis{i:02d}"]
-        return ",".join(header_array)
+        return "timestamp," + ",".join(header_array)
 
     def save(self) -> None:
         """Write the most current data to the disk."""
